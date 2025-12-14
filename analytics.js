0f13e6275df9c333b1105384a44bb6aff8e8da58
Aazar <48023643+44za12@users.noreply.github.com>
2025-12-14 15:26:20 +0400

    Fix analytics

---
+++ analytics.js
@@ -0,0 +1,11 @@
+// Analytics bootstrap (kept external to avoid CSP 'unsafe-inline')
+// Mouseflow
+window._mfq = window._mfq || [];
+(function () {
+  var mf = document.createElement('script');
+  mf.type = 'text/javascript';
+  mf.defer = true;
+  mf.src = 'https://cdn.mouseflow.com/projects/781aa7b5-afe9-4aef-95a0-47d1913e132f.js';
+  var head = document.getElementsByTagName('head')[0];
+  if (head) head.appendChild(mf);
+})();
