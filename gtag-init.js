95bc11f1a6c28d49a938cf6aa909d1d81c2a94da
Aazar <48023643+44za12@users.noreply.github.com>
2025-12-14 16:16:29 +0400

    ADD GTAG

--- gtag-init.js
+++
@@ -1,7 +0,0 @@
-// Google Analytics (GA4) initialization
-// Kept as an external file to avoid CSP 'unsafe-inline'.
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-ECCCJ3Y0TQ');
